e5bde8077802e759a74cf7398d845f9a8b1299b5
lvbubi <lvbubi2@gmail.com>
2021-10-29 23:57:41 +0200

    add objectmapper to mappercomponent

--- src/app/chart-builder/components/data-source-selector/data-source-mapper/data-source-mapper.component.ts
+++ src/app/chart-builder/components/data-source-selector/data-source-mapper/data-source-mapper.component.ts
@@ -1,6 +1,17 @@
 import { Component, EventEmitter, Input, OnInit, ViewChild } from '@angular/core';
 import { CdkTextareaAutosize } from "@angular/cdk/text-field";
 
+import * as objectMapper from 'object-mapper'
+
+import {
+  SingleSeries,
+  MultiSeries,
+  BubbleChartMultiSeries,
+  BoxChartMultiSeries,
+  Series,
+  TreeMapData
+} from '@swimlane/ngx-charts';
+
 @Component({
   selector: 'app-data-source-mapper',
   templateUrl: './data-source-mapper.component.html',
@@ -31,6 +42,23 @@ export class DataSourceMapperComponent implements OnInit {
    }]
    */
   setDataSource() {
-    this.resultEvent.emit(() => JSON.parse(this.textArea));
+    try {
+      let resultObject: SingleSeries | MultiSeries | BubbleChartMultiSeries | BoxChartMultiSeries | Series | TreeMapData = JSON.parse(this.textArea);
+      this.resultEvent.emit(() => resultObject);
+    } catch (e) {
+      console.log('failed to map', e);
+    }
+  }
+
+  /*
+   {"[].value": "asd[]"}
+   {"value": "asd"}
+   */
+  mapDataSource() {
+    //src, dest, map
+    let result = {asd: 'lol'};
+    console.log(JSON.parse(this.textArea), result, JSON.parse(this.mapper));
+    let mappedObject = objectMapper(JSON.parse(this.textArea), JSON.parse(this.mapper));
+    console.log(mappedObject, "mapped object");
   }
 }
