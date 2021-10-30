385ecf5e7d8401d7b4839cc83cd3cc9e1a6464a5
lvbubi <lvbubi2@gmail.com>
2021-10-30 23:19:13 +0200

    download configuration to json

--- src/app/chart-builder/components/export-options/export-options.component.ts
+++ src/app/chart-builder/components/export-options/export-options.component.ts
@@ -8,6 +8,7 @@ import { chartTypeSelector } from "../../store/chart.selectors";
 import chartGroups from "../../chartTypes";
 import { MAT_DIALOG_DATA, MatDialog } from "@angular/material/dialog";
 import { ChartRegisterService } from "../../service/chart-register.service";
+import { DomSanitizer } from "@angular/platform-browser";
 
 export interface DialogData {
   name: string,
@@ -23,22 +24,41 @@ export class ExportOptionsComponent implements OnInit {
 
   @Input() options: ChartOptions;
 
+  sanitizedBlobUrl: any;
+
   $chartType: Observable<string>;
   chartType: string;
   chartGroups: any = chartGroups;
 
-  exportedOptions: [][];
-
-  constructor(private store: Store<State>, private chartRegisterService: ChartRegisterService, public dialog: MatDialog) {
+  constructor(private store: Store<State>, private chartRegisterService: ChartRegisterService, public dialog: MatDialog, private sanitizer: DomSanitizer) {
     this.$chartType = store.select(chartTypeSelector);
     this.$chartType.subscribe(chartType => this.chartType = chartType);
   }
 
   ngOnInit(): void {
+    console.log("oninitshit")
   }
 
   exportOptions() {
-    this.exportedOptions = this.chartGroups.filter(group => !group.disabled)
+    let chartOptions = this.mapOptionsToChartType();
+    let chartOptionsString = JSON.stringify(chartOptions, null, 2);
+
+    this.chartRegisterService.addNewConfiguration(this.chartType, chartOptionsString);
+
+    let blob = new Blob([chartOptionsString], {type: 'application/json'});
+    this.sanitizedBlobUrl = this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(blob));
+  }
+
+  viewOptions() {
+    let chartOptions = this.mapOptionsToChartType();
+    this.dialog.open(ExportOptionsDialog, {
+        data: chartOptions
+      }
+    );
+  }
+
+  private mapOptionsToChartType() {
+    return this.chartGroups.filter(group => !group.disabled)
       .flatMap(group => group.charts)
       .filter(chart => chart.selector === this.chartType)
       .flatMap(chart => chart.options)
@@ -48,15 +68,6 @@ export class ExportOptionsComponent implements OnInit {
           value: JSON.stringify(this.options[option])
         };
       });
-
-    this.chartRegisterService.addNewConfiguration(this.chartType, JSON.stringify(this.exportedOptions));
-
-
-
-    this.dialog.open(ExportOptionsDialog, {
-        data: this.exportedOptions
-      }
-    );
   }
 }
 
